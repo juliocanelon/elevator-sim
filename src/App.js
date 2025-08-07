aa8df37bd916a4057b2b69d7c41d1ab4018e8e19
Julio Cesar <juliocanelon@gmail.com>
2025-08-07 02:34:47 -0400

    feat: add academic project header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ElevatorSimulation from './components/ElevatorSimulation';
+import Header from './components/Header';
 import { ElevatorProvider } from './context/ElevatorContext';
 
 function App() {
   return (
     <ElevatorProvider>
+      <Header />
       <ElevatorSimulation />
     </ElevatorProvider>
   );
